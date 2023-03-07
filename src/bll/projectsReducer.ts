e839c776cde54179887040373a40b527764ebbed
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2023-03-07 13:22:52 +0300

    Added Test projects.

--- src/bll/projectsReducer.ts
+++ src/bll/projectsReducer.ts
@@ -23,7 +23,8 @@ export type ToolType =
     | 'Formik'
     | 'EmailJS'
     | 'MUI'
-    | 'Reselect';
+    | 'Reselect'
+    | 'OOP';
 export type RatingType = 0 | 1 | 2 | 3 | 4 | 5;
 type RatingStateType = {
     averageRating: number
@@ -121,9 +122,54 @@ const projectsInitState: ProjectsStateType = {
             description: 'My first project in JavaScript. My best score is 33. Try to win!',
             codeLink: 'https://github.com/aliaksandr-batskalevich/jsForChildren-snakeGame',
             viewLink: 'https://aliaksandr-batskalevich.github.io/jsForChildren-snakeGame',
-            tools: ["HTML", "CSS", "JavaScript"],
+            tools: ["HTML", "CSS", "JavaScript", "OOP"],
             rating: {
                 averageRating: 4.2,
+                currentRating: null,
+                dateCurrentRatingAdd: null,
+            },
+            comments: '',
+        },
+        {
+            id: v1(),
+            title: '15puzzle',
+            image: 'https://www.i-igrushki.ru/upload/iblock/89f/89f17928b3c2ad478867a2e3f9b9c67f.jpg',
+            description: 'The legendary Fifteen-Puzzle game.',
+            codeLink: 'https://github.com/aliaksandr-batskalevich/fifteen-puzzle',
+            viewLink: 'https://aliaksandr-batskalevich.github.io/fifteen-puzzle/',
+            tools: ["HTML", "SCSS", "TypeScript", "React", "Redux"],
+            rating: {
+                averageRating: 5,
+                currentRating: null,
+                dateCurrentRatingAdd: null,
+            },
+            comments: '',
+        },
+        {
+            id: v1(),
+            title: 'Checkers',
+            image: 'https://shashki.ru/wp-content/uploads/2016/11/draughts_online_600x400.jpg',
+            description: 'Game of checkers. Implemented two levels of complexity of the logic of computer moves.',
+            codeLink: 'https://github.com/aliaksandr-batskalevich/checkers',
+            viewLink: 'https://aliaksandr-batskalevich.github.io/checkers/',
+            tools: ["HTML", "SCSS", "TypeScript", "React", "Redux", "OOP", "Formik", "Reselect"],
+            rating: {
+                averageRating: 4.5,
+                currentRating: null,
+                dateCurrentRatingAdd: null,
+            },
+            comments: '',
+        },
+        {
+            id: v1(),
+            title: 'Elevator',
+            image: 'https://static.mk.ru/upload/entities/2020/02/11/22/articles/detailPicture/3e/4b/18/c5/1a22a8e8c3e46525ab2573b208ed7c4c.jpg',
+            description: 'Logic of the elevator with visualization through React.',
+            codeLink: 'https://github.com/aliaksandr-batskalevich/elevator',
+            viewLink: 'https://aliaksandr-batskalevich.github.io/elevator/',
+            tools: ["HTML", "SCSS", "TypeScript", "React", "Redux"],
+            rating: {
+                averageRating: 4,
                 currentRating: null,
                 dateCurrentRatingAdd: null,
             },
